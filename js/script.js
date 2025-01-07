150e7dfc737ac2a40b9d7aa9acec54993d9cdd01
Nico <nicola.annese07@gmail.com>
2025-01-07 09:36:15 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
-let elements = document.querySelectorAll(".myElement");
+let elements = document.querySelectorAll(".myElement")
 
 for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener("click", function(event) {
-        event.currentTarget.classList.remove("greenBg");
-        event.currentTarget.classList.add("transition");
-        event.currentTarget.classList.add("yellowBg");
+        event.currentTarget.classList.remove("greenBg")
+        event.currentTarget.classList.add("transition")
+        event.currentTarget.classList.add("yellowBg")
     });
 }
