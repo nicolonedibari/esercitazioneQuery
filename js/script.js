65bbae0865389dd472b83907bbd59981e4b286d1
Nico <nicola.annese07@gmail.com>
2025-01-07 09:02:02 +0100

    implementazione

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+let elements = document.querySelectorAll(".myElement");
+
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", function(event) {
+        event.currentTarget.classList.remove("greenBg");
+        event.currentTarget.classList.add("transition");
+        event.currentTarget.classList.add("yellowBg");
+    });
+}
